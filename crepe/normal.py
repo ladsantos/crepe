4d038e0c98a048ee0433b54aa3aac3b8d74332c5
laugustogs <laugustogs@gmail.com>
2015-02-23 14:17:40 -0300

    PEP 8 style adjustment

--- crepe/normal.py
+++ crepe/normal.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 
-# Single-variate normal distributions #####################################
+# Single-variate normal distributions ########################################
 
 class optimize(object):
     
@@ -13,12 +13,15 @@ class optimize(object):
     
     # Optional:
     # N = number of samples to be drawn. Default = 100
-    # rho = sample lower quantile that will define the elite results. Default = 0.1
-    # c_limit = lower limit for the relative change in the performance. Default = 0.1
+    # rho = sample lower quantile that will define the elite results. 
+        # Default = 0.1
+    # c_limit = lower limit for the relative change in the performance. 
+        # Default = 0.1
     # k_max = maximum number of iterations. Default = 50
     # alpha = smoothing factor, between 0 and 1. Default = 1.0
     # beta = smoothing level, between ~0.1 and ~0.2 Default = 0.1
-    # silent = True or False. Make it True to not print anything on the terminal. Default = False
+    # silent = True or False. Make it True to not print anything on the 
+        # terminal. Default = False
     
     def estimate(self,perf,p_mean,p_sigma,**kwargs):
         
@@ -70,8 +73,10 @@ class optimize(object):
         while check > self.c_limit and k < self.k_max:
             
             k += 1
-            self.p = np.array([[p_mean[i] + p_sigma[i]*np.random.normal() \
-                for i in range(PN)] for j in range(self.N)])
+            self.p = np.array([
+                [p_mean[i] + p_sigma[i]*np.random.normal() \
+                for i in range(PN)] for j in range(self.N)
+            ])
             self.S = np.array([perf(self.p[j,:]) for j in range(self.N)])
             self.sorted_S = np.sort(self.S)
             self.q = self.sorted_S[np.int(self.N*self.rho)]
@@ -86,14 +91,18 @@ class optimize(object):
                 
             # Calculating the new parameters mean vector
             self.p_mean_prev = p_mean
-            p_mean = np.array([np.sum(self.I*self.w*self.p[:,j])/np.sum(self.I*self.w) \
-                for j in range(PN)])
+            p_mean = np.array([
+                np.sum(self.I*self.w*self.p[:,j])/np.sum(self.I*self.w) \
+                for j in range(PN)
+            ])
             p_mean = self.alpha*p_mean + (1.-self.alpha)*self.p_mean_prev
             
             # Calculating the new sigmas
             self.sigma_prev = p_sigma
-            p_sigma = np.array([np.sqrt(np.sum(self.I*self.w*(p_mean[j]-self.p[:,j])**2)/np.sum(self.I*self.w)) \
-                for j in range(PN)])
+            p_sigma = np.array([
+                np.sqrt(np.sum(self.I*self.w*(p_mean[j]-self.p[:,j])**2)/np.sum(self.I*self.w)) \
+                for j in range(PN)
+            ])
             self.alpha_d = self.alpha-self.alpha*(1-k**(-1))**(1.0/self.beta)
             p_sigma = self.alpha_d*p_sigma + (1.0-self.alpha_d)*self.sigma_prev
                 
