df4285905a191e205012108a5b457ca9a78f6a79
laugustogs <laugustogs@gmail.com>
2015-02-23 15:06:58 -0300

    added more comments and verbose option

--- crepe/normal.py
+++ crepe/normal.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import numpy as np
+###########################################################################
+# This code calculates the best estimation for parameters of a given model.
+# These parameters have to be independent of each other, or in other words,
+# they follow single-variate Gaussian distributions.
+###########################################################################
 
-# Single-variate normal distributions ########################################
+import numpy as np
 
 class optimize(object):
     
@@ -57,10 +61,10 @@ class optimize(object):
         else:
             self.beta = 0.1
         
-        if ('silent' in kwargs):
-            self.silent = kwargs['silent']
+        if ('verbose' in kwargs):
+            self.verbose = kwargs['verbose']
         else:
-            self.silent = False
+            self.verbose = False
         
         # Starting a few helpful variables
         check = 1.0
@@ -72,22 +76,37 @@ class optimize(object):
         
         while check > self.c_limit and k < self.k_max:
             
-            k += 1
+            k += 1      # iteration counter
+            
+            # Drawing a sample from a normal distribution
             self.p = np.array([
                 [p_mean[i] + p_sigma[i]*np.random.normal() \
                 for i in range(PN)] for j in range(self.N)
             ])
+            
+            # Following lines:
+            # 1) Performance evaluation
+            # 2) Sort the performances
+            # 3) Get a sample quantile (which will be the elite)
+            # 4) Calculate the relative changes
+            # 5) Re-evaluates q0
+            # 6) Get the indices of the elite sample
+            # 7) Get the inverse squared performances (used in calculation)
             self.S = np.array([perf(self.p[j,:]) for j in range(self.N)])
             self.sorted_S = np.sort(self.S)
             self.q = self.sorted_S[np.int(self.N*self.rho)]
-            check = np.abs(self.q-q0)/self.q
+            check = np.abs(self.q-q0)/self.q    
             q0 = self.q
             self.ind = np.nonzero(self.S < self.q)
             self.w = self.S**(-2)
             
+            if self.verbose == True:
+                print "Iteration %i, change = %.3E" % (k,check)
+            
             self.I = np.zeros(self.N,float)
             for i in range(len(self.ind)):
                 self.I[self.ind[i]] = 1.0
+            # Matrix I selects the elite sample (by multiplication)
                 
             # Calculating the new parameters mean vector
             self.p_mean_prev = p_mean
@@ -106,9 +125,9 @@ class optimize(object):
             self.alpha_d = self.alpha-self.alpha*(1-k**(-1))**(1.0/self.beta)
             p_sigma = self.alpha_d*p_sigma + (1.0-self.alpha_d)*self.sigma_prev
                 
-        if self.silent == False and k == self.k_max:
+        if self.verbose == True and k == self.k_max:
             print 'Max iteration limit reached.'
-        elif self.silent == False and k < self.k_max:
+        elif self.verbose == True and k < self.k_max:
             print "CREPE's number of iterations = %i" % k
         
         return p_mean,p_sigma
